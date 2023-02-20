6e9ba9537c4f3caf9a7f3dece478da55b3658012
Prerana-Prashant <125781921+Prerana-Prashant@users.noreply.github.com>
2023-02-20 14:23:37 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,7 +6,7 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
-  cout << "Hi PES1UG20CS312\n";
-  cout << "How are you all?\n";
+  cout << "Hi I'm making changes\n";
+  cout << "How are you all\n";
   return 0;
 }
